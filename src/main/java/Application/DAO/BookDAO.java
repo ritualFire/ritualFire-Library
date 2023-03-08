020a17f8d434f93cb5d38f045f4ef4b16c004830
ritualFire <jpelaez456@gmail.com>
2023-03-08 02:41:37 +0000

    Library

--- src/main/java/Application/DAO/BookDAO.java
+++ src/main/java/Application/DAO/BookDAO.java
@@ -32,7 +32,7 @@ public class BookDAO {
         List<Book> books = new ArrayList<>();
         try {
             //Write SQL logic here
-            String sql = "change me";
+            String sql = "Select * From book";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             ResultSet rs = preparedStatement.executeQuery();
             while(rs.next()){
@@ -57,10 +57,12 @@ public class BookDAO {
         Connection connection = ConnectionUtil.getConnection();
         try {
             //Write SQL logic here
-            String sql = "change me";
+            String sql = "Select * From book Where isbn = ?";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
             //write preparedStatement's setInt method here.
+
+            preparedStatement.setInt(1,isbn);
 
             ResultSet rs = preparedStatement.executeQuery();
             while(rs.next()){
@@ -87,10 +89,16 @@ public class BookDAO {
         Connection connection = ConnectionUtil.getConnection();
         try {
             //Write SQL logic here
-            String sql = "change me" ;
+            String sql = "Insert Into book Values(?,?,?,?)" ;
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
             //write preparedStatement's setString and setInt methods here.
+
+            preparedStatement.setInt(1,book.getIsbn());
+            preparedStatement.setInt(2,book.getAuthor_id());
+            preparedStatement.setString(3,book.getTitle());
+            preparedStatement.setInt(4,book.getCopies_available());
+
 
             preparedStatement.executeUpdate();
             return book;
@@ -109,7 +117,7 @@ public class BookDAO {
         List<Book> books = new ArrayList<>();
         try {
             //Write SQL logic here
-            String sql = "change me";
+            String sql = "Select * From book Where copies_available > 0";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
             //write preparedStatement's setInt method here.
